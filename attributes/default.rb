fa735d3219adca808ac6aa383bae92c59f2c7b52
DQNEO <dqneoo@gmail.com>
2014-03-16 16:43:10 +0900

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['emacs']['version'] = '24.3'
+default['emacs']['prefix'] = '/usr/local'
+default['emacs']['url'] = 'ftp://ftp.ring.gr.jp/pub/GNU/emacs/'
---
+++ recipes/default.rb
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# see http://dqn.sakusakutto.jp/2012/06/centos62emacs241install.html
+tarball_filename = "emacs-#{node['emacs']['version']}.tar.gz"
+tarball_filepath = Chef::Config['file_cache_path'] + '/' + tarball_filename
+remote_filepath = node['emacs']['url'] + tarball_filename
+
+package "ncurses-devel" do
+  action :install
+end
+
+remote_file tarball_filepath do
+  source remote_filepath
+  mode      00644
+end
+
+# reduce line-noise-eyness
+execute "Extracting and Building Emacs #{node['emacs']['version']} from Source" do
+  cwd Chef::Config['file_cache_path']
+  command <<-COMMAND
+    tar xfz ${tarball_filename}
+    cd emacs-#{node['emacs']['version']}
+    ./configure --without-x
+    make
+    make install
+  COMMAND
+  creates "#{node['emacs']['prefix']}/bin/emacs"
+  not_if "emacs --version | grep #{node['emacs']['version']}"
+end
