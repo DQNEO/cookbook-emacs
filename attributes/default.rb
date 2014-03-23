13af6029e012bc090a1e7bf0a9d4b0f8235c789f
DQNEO <dqneoo@gmail.com>
2014-03-23 16:29:48 +0900

    change from ring  to jaist because ring server was down

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['emacs']['version'] = '24.3'
 default['emacs']['prefix'] = '/usr/local'
-default['emacs']['url'] = 'ftp://ftp.ring.gr.jp/pub/GNU/emacs/'
+#default['emacs']['url'] = 'ftp://ftp.ring.gr.jp/pub/GNU/emacs/'
+default['emacs']['url'] = 'ftp://ftp.jaist.ac.jp/pub/GNU/emacs/'
